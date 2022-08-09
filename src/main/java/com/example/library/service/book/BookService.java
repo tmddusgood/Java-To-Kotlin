fa0d97a110552b871fd06d564429148b9977a19a
tmddusgood <tmddusgood@gmail.com>
2022-08-10 02:47:09 +0900

    Add Service BookService

---
+++ src/main/java/com/example/library/service/book/BookService.java
@@ -0,0 +1,54 @@
+package com.example.library.service.book;
+
+import com.example.library.domain.book.Book;
+import com.example.library.domain.book.BookRepository;
+import com.example.library.domain.user.User;
+import com.example.library.domain.user.UserRepository;
+import com.example.library.domain.user.UserLoanHistoryRepository;
+import com.example.library.dto.book.request.BookLoanRequest;
+import com.example.library.dto.book.request.BookRequest;
+import com.example.library.dto.book.request.BookReturnRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class BookService {
+
+  private final BookRepository bookRepository;
+  private final UserRepository userRepository;
+  private final UserLoanHistoryRepository userLoanHistoryRepository;
+
+  public BookService(
+      BookRepository bookRepository,
+      UserRepository userRepository,
+      UserLoanHistoryRepository userLoanHistoryRepository
+  ) {
+    this.bookRepository = bookRepository;
+    this.userRepository = userRepository;
+    this.userLoanHistoryRepository = userLoanHistoryRepository;
+  }
+
+  @Transactional
+  public void saveBook(BookRequest request) {
+    Book newBook = new Book(request.getName());
+    bookRepository.save(newBook);
+  }
+
+  @Transactional
+  public void loanBook(BookLoanRequest request) {
+    Book book = bookRepository.findByName(request.getBookName()).orElseThrow(IllegalArgumentException::new);
+    if (userLoanHistoryRepository.findByBookNameAndIsReturn(request.getBookName(), false) != null) {
+      throw new IllegalArgumentException("진작 대출되어 있는 책입니다");
+    }
+
+    User user = userRepository.findByName(request.getUserName()).orElseThrow(IllegalArgumentException::new);
+    user.loanBook(book);
+  }
+
+  @Transactional
+  public void returnBook(BookReturnRequest request) {
+    User user = userRepository.findByName(request.getUserName()).orElseThrow(IllegalArgumentException::new);
+    user.returnBook(request.getBookName());
+  }
+
+}
