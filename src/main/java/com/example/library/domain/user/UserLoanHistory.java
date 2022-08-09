3809832a9ed9dd1073f395ac708e61ffb5b1a1d7
tmddusgood <tmddusgood@gmail.com>
2022-08-09 23:35:56 +0900

    Add Domain User

---
+++ src/main/java/com/example/library/domain/user/UserLoanHistory.java
@@ -0,0 +1,40 @@
+package com.example.library.domain.user;
+import com.example.library.domain.user.User;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+public class UserLoanHistory {
+
+  @Id
+  @GeneratedValue(strategy = IDENTITY)
+  private Long id;
+
+  @ManyToOne
+  private User user;
+  private String bookName;
+  private boolean isReturn;
+
+  public UserLoanHistory() {
+  }
+
+  public UserLoanHistory(User user, String bookName, boolean isReturn) {
+    this.user = user;
+    this.bookName = bookName;
+    this.isReturn = isReturn;
+  }
+
+  public String getBookName() {
+    return this.bookName;
+  }
+
+  public void doReturn() {
+    this.isReturn = true;
+  }
+
+}
---
+++ src/main/java/com/example/library/domain/user/UserLoanHistoryRepository.java
@@ -0,0 +1,7 @@
+package com.example.library.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserLoanHistoryRepository extends JpaRepository<UserLoanHistory, Long> {
+  UserLoanHistory findByBookNameAndIsReturn(String bookName, boolean isReturn);
+}
