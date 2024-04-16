97a7afeda1a52706a1f730d69e884bfb3c758c1d
tmddusgood <tmddusgood@gmail.com>
2024-04-16 19:57:31 +0900

    add: 추가 WeatherServiceApplication @EnableJPAAuditing

--- src/main/kotlin/com/misoweather/weatherservice/WeatherServiceApplication.kt
+++ src/main/kotlin/com/misoweather/weatherservice/WeatherServiceApplication.kt
@@ -3,9 +3,11 @@ package com.misoweather.weatherservice
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableJpaAuditing
 class WeatherServiceApplication
 
 fun main(args: Array<String>) {
